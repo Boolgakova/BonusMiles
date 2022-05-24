74d12fed0d6dd5465b6bc9a1760a4289f9113e9c
Boolgakova <boolgakova@gmail.com>
2022-05-24 16:52:46 +0300

    Name of variable fixed

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket_price = 15476; //стоимость билета
+        int ticketPrice = 15476; //стоимость билета
         int mile = 20; //стоимость 1 бонусной мили
 
-        int bonus =  ticket_price / mile;
+        int bonus =  ticketPrice / mile;
         System.out.println("Вам начислено "+bonus+" бонусных миль");
     }
 }
